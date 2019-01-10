1c590b621ee1d4ee94a9881d6b1512c0cdbf83c2
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 18:10:17 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,56 +1,14 @@
-languages = {
-  :oo => {
-    :ruby => {
-      :type => "interpreted"
-    },
-    :javascript => {
-      :type => "interpreted"
-    },
-    :python => {
-      :type => "interpreted"
-    },
-    :java => {
-      :type => "compiled"
-    }
-  },
-  :functional => {
-    :clojure => {
-      :type => "compiled"
-    },
-    :erlang => {
-      :type => "compiled"
-    },
-    :scala => {
-      :type => "compiled"
-    },
-    :javascript => {
-      :type => "interpreted"
-    }
- 
-  }
-}
-
 def reformat_languages(languages)
   reformatted = {}
   languages.each do |style, language_info|
     language_info.each do |langs, attributes|
-      puts langs
       if reformatted[langs] == nil
         reformatted[langs] = attributes
         reformatted[langs][:style] = [style]
       else
-        puts "ELSE #{langs}"
         reformatted[langs][:style] << style
-#        if reformatted[langs][:style] == nil
-#          reformatted[langs][:style] = [style]
-#        else
-#          puts "ELSE"
-#          reformatted[langs][:style] << style
       end
-#      puts "Additional: #{reformatted[langs]}"
     end
   end
-  puts reformatted
+  reformatted
 end
-
-reformat_languages(languages)
