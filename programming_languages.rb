5e6f2ff5aa3f90fe3429d58e95126494701feb2c
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 18:03:48 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -35,7 +35,7 @@ def reformat_languages(languages)
   languages.each do |style, language_info|
     language_info.each do |langs, attributes|
       reformatted[langs] = attributes
-      puts "Initial: #{reformatted[langs][:style}"
+      puts "Initial: #{reformatted[langs][:style]}"
       if reformatted[langs][:style] == nil
         reformatted[langs][:style] = [style]
       else
